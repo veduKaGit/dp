28921e2dfda7a1562e2f357bd6cfcdf1ca2f058f
Vedant Gupta <88966831+95135vedantgupta@users.noreply.github.com>
2022-04-23 01:03:12 +0530

    Update 13 Coin change problem_ Minimum number of coin.cpp

--- Dynamic Programming/13 Coin change problem_ Minimum number of coin.cpp
+++ Dynamic Programming/13 Coin change problem_ Minimum number of coin.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define INF INT_MAX-1
+#define INF INT_MAX-1  //like for comparision in max function, we take the value zero // same way for comparing with min, we took value INT_MAX
+//why INT_MAX-1 ????...see code..later we did +1..now numbers in computers are cyclic....so INT_MAX+1 will give a -ive value...thus we did (INT_MAX-1) initially
 
 int getMinNumberOfCoins(int coins[], int n, int sum) {
 	int t[n + 1][sum + 1];
@@ -25,7 +26,7 @@ int getMinNumberOfCoins(int coins[], int n, int sum) {
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= sum; j++)
 			if (coins[i - 1] <= j)
-				t[i][j] = min(t[i - 1][j], 1 + t[i][j - coins[i - 1]]);
+				t[i][j] = min(t[i - 1][j], 1 + t[i][j - coins[i - 1]]);  //here we take min...not max  // also here we did 1+t[][] because we are adding 1 coin
 			else
 				t[i][j] = t[i - 1][j];
 
