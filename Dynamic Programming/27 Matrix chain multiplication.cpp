58ec56996b5c53b9e18edb1bfb66ae83e580e53d
Vedant Gupta <88966831+95135vedantgupta@users.noreply.github.com>
2022-05-04 00:16:31 +0530

    Update 27 Matrix chain multiplication.cpp

--- Dynamic Programming/27 Matrix chain multiplication.cpp
+++ Dynamic Programming/27 Matrix chain multiplication.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int Solve(int arr[], int i, int j) {
-	if (i >= j)
+	if (i >= j)  //base condn...will NOT work for i==j also....since only 1 sized array can NOT give dimensions of a matrix (we need atleast 2 elements to give size of a matrix)...thus j>i is compulsory 
 		return 0;
 
 	int ans = INT_MAX;
-	for (int k = i; k <= j - 1; k++) {
-		int temp_ans = Solve(arr, i, k) + Solve(arr, k + 1, j) + arr[i - 1] * arr[k] * arr[j];
+	for (int k = i; k <= j - 1; k++) {  //IMP....see constraints on k // there are 2 sets formed...(i to k) and (k+1 to j)
+		int temp_ans = Solve(arr, i, k) + Solve(arr, k + 1, j) + arr[i - 1] * arr[k] * arr[j]; //main logic
 		ans = min(ans, temp_ans); // take temp minimum value from the temp answers
 	}
 
@@ -20,6 +20,6 @@ signed main() {
 	for (int i = 0; i < n; i++)
 		cin >> arr[i];
 
-	cout << Solve(arr, 1, n - 1) << endl;
+	cout << Solve(arr, 1, n - 1) << endl;  //i can NOT start from 0...since dimensions of matrix are arr[i-1]*arr[i]
 	return 0;
 }
