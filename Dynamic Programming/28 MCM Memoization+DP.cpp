ad371b4a11bcfa069e50db1122224b10de3e050e
Vedant Gupta <88966831+95135vedantgupta@users.noreply.github.com>
2022-05-07 10:27:26 +0530

    Update 28 MCM Memoization+DP.cpp

--- Dynamic Programming/28 MCM Memoization+DP.cpp
+++ Dynamic Programming/28 MCM Memoization+DP.cpp
@@ -4,8 +4,8 @@ using namespace std;
 const int D = 1000;
 int t[D][D];
 
-int Solve(int arr[], int i, int j) {
-	if (i >= j) {
+int Solve(int arr[], int i, int j) {  
+	if (i >= j) {  //corner case
 		t[i][j] = 0;
 		return 0;
 	}
@@ -36,23 +36,25 @@ signed main() {
 
 
 
+
 //the dp approach is below:
 
 int MatrixChainOrder(int p[], int n)
 {
 
-	/* For simplicity of the program, one
-	extra row and one extra column are
-	allocated in m[][].
-	--> 0th row and 0th column of m[][] are not used */
+	//we looked at the function Solve in recursion...its parameters i,j keep on changing with every function call
+	//thus the matrix will be formed with 1 side representing i, and the other representing j
+	//For simplicity of the program, one
+ 	//extra row and one extra column are allocated in m[][].
+	//--> 0th row and 0th column of m[][] are not used 
 	int m[n][n];
 
 	int i, j, k, L, q;
 
     // DIAGONAL is set =0 since when i==j, 
     //theres only 1 element...which can not form a matrix
-    for (i = 1; i < n; i++)
-		m[i][i] = 0;
+    for (i = 1; i < n; i++)  //1 triangular half of the matrix will be COMPLETELY ZERO , since base condition (i>=j -->return 0)
+		m[i][i] = 0; //but that triangular half will not be used anywhere in our calculations since the diagonal will be used
 
 	// L is chain length which we consider
 	// whenever we have a length L, i can be from (1 to n-L) and j will be (i+L-1)
@@ -68,13 +70,13 @@ int MatrixChainOrder(int p[], int n)
 			for (k = i; k <= j - 1; k++)//3 loops...logical...in memoization...1 foor loop (extra)...so tabular approach also has 1 extra loop
 			{
 				q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-				if (q < m[i][j])
+				if (q < m[i][j])  //since for each i,j...we will hv many values since k changes
 					m[i][j] = q;
 			}
 		}
 	}
 
-	return m[1][n - 1]; //NOT m[n-1][n-1] //**IMP**
+	return m[1][n - 1]; //NOT m[n-1][n-1] //*************IMP**************
 }
 
 
