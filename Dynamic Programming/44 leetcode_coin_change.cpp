dea4f5b53ea3597883fbba61b6630d31462ce5c3
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-06 00:05:21 +0530

    Create 44 leetcode_coin_change.cpp

---
+++ Dynamic Programming/44 leetcode_coin_change.cpp
@@ -0,0 +1,21 @@
+// question:
+// https://leetcode.com/problems/coin-change/
+
+//i did in O(n^2)....gave TLE
+
+//this logic: O(n * size_of_coins_vector)
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+ 
+        vector<int>dp(amount+1,amount+1000);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++)
+            for(int j=0;j<coins.size();j++)
+                if(i-coins[j]>=0)
+                    dp[i]=min(dp[i],1+dp[i-coins[j]]);        
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+};
