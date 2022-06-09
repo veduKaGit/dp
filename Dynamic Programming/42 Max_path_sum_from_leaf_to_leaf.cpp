d2d72421f0118cb4009d074190ae20ffe95240bb
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-09 13:36:02 +0530

    Update 42 Max_path_sum_from_leaf_to_leaf.cpp

--- Dynamic Programming/42 Max_path_sum_from_leaf_to_leaf.cpp
+++ Dynamic Programming/42 Max_path_sum_from_leaf_to_leaf.cpp
@@ -1,5 +1,3 @@
-//here ans handles only the case when left and right nodes are not null
-
 class Solution {
 public:
    int ans;
@@ -11,10 +9,10 @@ public:
        int l = solve(root->left); //max sum starting from the root->left node till leaf node in left sub tree
        int r = solve(root->right); //max sum starting from the root->right node till leaf node in right sub tree
 
-       if(root->left!=NULL and root->right!=NULL)
+       if(root->left!=NULL and root->right!=NULL) //considering that max sum from leaf to leaf is through the CURRENT node
            ans = max(ans, l+r+root->data); 
        
-       if(root->left==NULL and root->right!=NULL)
+       if(root->left==NULL and root->right!=NULL)  //this if, else if, else is considering that max sum from leaf to leaf is through the PARENT node
            return root->data + r;
        else if(root->left!=NULL and root->right==NULL)
            return root->data + l;
