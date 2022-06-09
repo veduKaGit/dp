c74213d6e414ba848b5c0377769f78ad025cde59
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-09 13:26:43 +0530

    Update 42 Max_path_sum_from_leaf_to_leaf.cpp

--- Dynamic Programming/42 Max_path_sum_from_leaf_to_leaf.cpp
+++ Dynamic Programming/42 Max_path_sum_from_leaf_to_leaf.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    int ans;
+   int ans;
    int solve(Node *root)
    {
        if(root==NULL)
            return 0;
        
-       int l = solve(root->left);
-       int r = solve(root->right);
+       int l = solve(root->left); //max sum starting from the root->left node till leaf node in left sub tree
+       int r = solve(root->right); //max sum starting from the root->right node till leaf node in right sub tree
 
        if(root->left!=NULL and root->right!=NULL)
-           ans = max(ans,l+r+root->data);
+           ans = max(ans, l+r+root->data); 
        
        if(root->left==NULL and root->right!=NULL)
            return root->data + r;
        else if(root->left!=NULL and root->right==NULL)
            return root->data + l;
-       else if((root->left!=NULL and root->right!=NULL) or (root->left==NULL and root->right==NULL))
+       else if((root->left!=NULL and root->right!=NULL) or (root->left==NULL and root->right==NULL))  //or simply do else...see commented part below
            return root->data + max(l,r);
+//        else
+//             return root->data + max(l,r);
    }
    int maxPathSum(Node* root)
    {
        ans = INT_MIN;
        int x = solve(root);
-       if(root->left and root->right)
+       if(root->left!=NULL and root->right!=NULL)
             return ans;
       
-       return max(ans,x);
+       return max(ans,x);//IMP....
    }
 };
