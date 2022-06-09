6d50c2b9db6db768ee7d9c45d817d082c1ce7ab9
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-09 13:09:06 +0530

    Update 42 Max_path_sum_from_leaf_to_leaf.cpp

--- Dynamic Programming/42 Max_path_sum_from_leaf_to_leaf.cpp
+++ Dynamic Programming/42 Max_path_sum_from_leaf_to_leaf.cpp
@@ -4,38 +4,28 @@ public:
    int solve(Node *root)
    {
        if(root==NULL)
-       {
            return 0;
-       }
+       
        int l = solve(root->left);
        int r = solve(root->right);
 
        if(root->left!=NULL and root->right!=NULL)
-       {
            ans = max(ans,l+r+root->data);
-       }
        
        if(root->left==NULL and root->right!=NULL)
-       {
            return root->data + r;
-       }
        else if(root->left!=NULL and root->right==NULL)
-       {
            return root->data + l;
-       }
        else if((root->left!=NULL and root->right!=NULL) or (root->left==NULL and root->right==NULL))
-       {
            return root->data + max(l,r);
-       }
    }
    int maxPathSum(Node* root)
    {
        ans = INT_MIN;
        int x = solve(root);
-      if(root->left and root->right)
-      {
-          return ans;
-      }
+       if(root->left and root->right)
+            return ans;
+      
        return max(ans,x);
    }
 };
