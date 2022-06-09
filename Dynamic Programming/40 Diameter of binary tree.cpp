a322b4c5dfcaba0a9503e2875d2c9848564fc1c6
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-09 11:38:30 +0530

    Update 40 Diameter of binary tree.cpp

--- Dynamic Programming/40 Diameter of binary tree.cpp
+++ Dynamic Programming/40 Diameter of binary tree.cpp
@@ -17,12 +17,12 @@ public:
         if (root == NULL)
             return 0;
 
-        int l = Solve(root->left);
-        int r = Solve(root->right);
+        int l = Solve(root->left);//returns max ht of left sub tree
+        int r = Solve(root->right);//returns max ht of right sub tree
 
-        int temp = 1 + max(l, r);
-        int ans = max(temp, l + r + 1);
-        res = max(res, ans);
+        int temp = (1+max(l, r));  //returns max height...considering max diameter is through the parent node
+        int ans = max(temp, l+r+1); //l+r+1 is the height considering max diameter is through current node
+        res = max(res, ans);//this is our main ans
         return temp;
     }
     int diameterOfBinaryTree(TreeNode* root) {
@@ -31,6 +31,6 @@ public:
 
         res = INT_MIN + 1;
         Solve(root);
-        return res - 1;
+        return res - 1; //IMP!!!! return res-1....depends from ques to ques to ques on what they consider the 'height'...
     }
 };
