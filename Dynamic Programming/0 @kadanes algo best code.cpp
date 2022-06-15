722f03cee8043bf6ca1cc984b778c544bc4a339f
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-16 00:36:02 +0530

    Update 0 @kadanes algo best code.cpp

--- Dynamic Programming/0 @kadanes algo best code.cpp
+++ Dynamic Programming/0 @kadanes algo best code.cpp
@@ -5,7 +5,7 @@ public:
         int cur_max = v[0], ans = v[0];
         for(int i = 1; i < n; i++)
         {
-            cur_max = max(v[i], cur_max + v[i]); //considersboth cases...when v[i] is +ive or -ive...consider 2 examples...{4 2 -1 -3 6} and {4 2 -5 -3 -1 9}...now dry run
+            cur_max = max(v[i], cur_max + v[i]);//considers both cases-> v[i] is +ive or -ive...consider 2 examples-> {4 2 -1 -3 6} and {4 2 -5 -3 -1 9}...now dry run
             ans = max(ans, cur_max); 
         }
         
