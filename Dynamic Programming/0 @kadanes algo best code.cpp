e9e6e675df66ef6ab5fb0f72bbf60f2c7d92d21c
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-18 15:12:21 +0530

    Update 0 @kadanes algo best code.cpp

--- Dynamic Programming/0 @kadanes algo best code.cpp
+++ Dynamic Programming/0 @kadanes algo best code.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxSubArray(vector<int>& v) {
        int n = v.size();
-        int cur_max = v[0], ans = v[0];
+        int cur_max = v[0], ans = v[0];  //2 variables....curr_max and ans....REMEMBER!
         for(int i = 1; i < n; i++)
         {
             cur_max = max(v[i], cur_max + v[i]);//considers both cases-> v[i] is +ive or -ive...consider 2 examples-> {4 2 -1 -3 6} and {4 2 -5 -3 -1 9}...now dry run
