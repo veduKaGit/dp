e5d9b9a0af2bf6f0e08c85d96ac3ac0e7743c0be
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-07-04 22:17:03 +0530

    Update 01 Recursive Knapsack.cpp

--- Dynamic Programming/01 Recursive Knapsack.cpp
+++ Dynamic Programming/01 Recursive Knapsack.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 int Knapsack(int wt[], int val[], int W, int n) {
-	// every recursive solution will have a base condition 
   // for base condition we need to think of the smallest valid input that we can pass 
   // array size can be atleast 0 || min weight can be 0 but not negetive; 
 	if (n == 0 || W == 0)
 		return 0;
 
-	// these are the choices we are having 
-	if (wt[n - 1] <= W) {
-		return max(val[n - 1] + Knapsack(wt, val, W - wt[n - 1], n - 1),
-		           Knapsack(wt, val, W, n - 1));
+	if (wt[n - 1] <= W) 
+	{
+		return max(val[n - 1] + Knapsack(wt, val, W - wt[n - 1], n - 1), //take it-> NOT val[n] and wt[n]...because indexing is from 0 to (n-1)
+		           Knapsack(wt, val, W, n - 1));  //dont take it
 	}
 	else if (wt[n - 1] > W) // if the weight is greater then the required weight there is no sence for taking that value. 
 		return Knapsack(wt, val, W, n - 1); // return as it is by redusing the size of array 
