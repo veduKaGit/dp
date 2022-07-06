8c0b71013bf507351e2cdedd3d8f6d18365d03ef
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-07-06 12:19:48 +0530

    Update 32 *IMP_QUES* Evaluate Expression to true Recursive.cpp

--- Dynamic Programming/32 *IMP_QUES* Evaluate Expression to true Recursive.cpp
+++ Dynamic Programming/32 *IMP_QUES* Evaluate Expression to true Recursive.cpp
@@ -1,10 +1,14 @@
 // eg given string=> "T|T&F^T" =>find no. of ways we put parenthesis so that expression evaluates to true
 
+//eg=> "T | F & F ^ T | T & F"
+//        i       k       j
+//     (         ) (         )  =>brackets this way =>around the operator
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int Solve(string X, int i, int j, bool isTrue) {
-	if (i >= j) {
+	if (i >= j) {   //base case...only 1 element
 		if (isTrue)
 			return X[i] == 'T';
 		else
@@ -13,10 +17,11 @@ int Solve(string X, int i, int j, bool isTrue) {
 
 	int ans = 0;
 	for (int k = i + 1; k <= j-1; k += 2) { // k=[i+1 , j-1] and k+=2 =>since we are iterating over the boolean operators=> operators ke left/right bracket lagega 
-		int l_T = Solve(X, i, k - 1, true);
-		int l_F = Solve(X, i, k - 1, false);
-		int r_T = Solve(X, k + 1, j, true);
-		int r_F = Solve(X, k + 1, j, false);
+		
+		int l_T = Solve(X, i, k - 1, true); //no. of ways left side expression of k is true
+		int l_F = Solve(X, i, k - 1, false); //no. of ways left side expression of k is false
+		int r_T = Solve(X, k + 1, j, true); //no. of ways right side expression of k is true
+		int r_F = Solve(X, k + 1, j, false); //no. of ways left side expression of k is false
 
 		if (X[k] == '|') {
 			if (isTrue == true)
