a8280f546858e1b55bc7c51d54470f6bdc7f4c17
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-03-15 23:13:22 +0530

    Update 39 **IMP OPTIMIZE** Egg dropping problem memoization optimization.cpp

--- Dynamic Programming/39 **IMP OPTIMIZE** Egg dropping problem memoization optimization.cpp
+++ Dynamic Programming/39 **IMP OPTIMIZE** Egg dropping problem memoization optimization.cpp
@@ -17,13 +17,13 @@ public:
         {
             int mid=(l+h)/2;
             
-            int left=find(k-1,mid-1,memo);   //egg broken-> check for down floors of mid -> since our required floor lies in this range
-            int right=find(k,n-mid,memo) ;   // not broken -> check for up floors of mid -> since our required floor lies in this range
-            temp = 1 + max(left,right);          //store max of both -> +1 because no. of moves increase by 1
+            int left=find(k-1,mid-1,memo);   //egg broken
+            int right=find(k,n-mid,memo) ;   // not broken  *** VVIMP *** => DONT do h-mid, it's n-mid
+            temp = 1 + max(left,right);      //store max of both -> +1 because no. of moves increase by 1
             
-            if(left<right)                  //since right is more than left and we need more in worst case 
-              l=mid+1;                       // so l=mid+1 to gain more temp for worst case : upward
-            else                             //left > right so we will go downward 
+            if(left<right)                  //we need MORE in worst case
+              l=mid+1;                       
+            else                            
                 h=mid-1;
             
             ans=min(ans,temp);               //store minimum attempts
