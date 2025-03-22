cc4ff91737de5936b2d2803f6c4013d4f78dc634
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-03-22 15:04:34 +0530

    Update 54 remove_boxes.cpp

--- Dynamic Programming/54 remove_boxes.cpp
+++ Dynamic Programming/54 remove_boxes.cpp
@@ -1,4 +1,8 @@
 https://leetcode.com/problems/remove-boxes/description/
 
+dp(l, r, k) = the maximum points we can get in indexes [l,r] 
+              if we have k boxes of the same color 
+              as boxes[l] in the left side of index l.
+
 ans:
 https://leetcode.com/problems/remove-boxes/solutions/4831819/3-d-dp-easy-detailed-c-solution/
