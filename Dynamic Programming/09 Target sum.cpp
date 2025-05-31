10e95991157a6cc2b60aa587fa017f6507fa8f76
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-05-31 13:55:48 +0530

    Update 09 Target sum.cpp

--- Dynamic Programming/09 Target sum.cpp
+++ Dynamic Programming/09 Target sum.cpp
@@ -1,82 +1,27 @@
-// For those who has problem with test case  [0,0,0,0,0,0,0,0,1], target = 1.
+// https://leetcode.com/problems/target-sum/description/
 
-// The solution doesn't consider presence of "0"s in the array. Why the output is different ?
+// in such questions, NEVER include base condition if(target==0)
+// because then we can miss subsets which are forming with trailing zero's
 
-// Because, if we have "0", then, it can have +0 and -0 and still will not effect the sum of a set. For example: Target value is = 2
-
-// 1) {0,2} = {+0,2}, {-0,2}.  Ans: 2
-
-// But if we increase number of 0s,
-
-// 2) {0,0,2} = {+0,+0,2}, {+0,-0,2}, {-0,+0,2},{-0,-0,2} . Ans: 4
-
-
-
-// So, if you observe, your answer increase by (2^number of 0s) i.e. pow(2,number of zeros).
-
-// So, make a small change as follows:
-
-// 1) on count of subsetsum function, 
-
-// if(nums[i-1] > j )    => change to:  if (nums[i-1] > j || nums[i-1] == 0)
-
-//       dp[i][j] = dp[i-1][j];
-
-//  //make no change and put the previous value as it is in the next subproblem. (I.e. 2, in example above)
-
-// And then at the end, you answer will be
-
-// return (int)Math.pow(2, number of 0s) * dp[nums.length][target] ;
-
-
-//ABOVE EXPLANATION IS CORRECT, BUT INSTEAD OF DOING ALL THIS, I SIMPLY MADE A CHANGE IN THE INITIALIZATION WHICH WORKS PERFECT:
-
+//instead use a single base condition if(i==n)
 
 class Solution {
 public:
-   int CountSubsetsWithSum(vector<int>& nums,int sum) {
-        int n= nums.size();
-	int t[n + 1][sum + 1]; // DP - matrix
-	// initialization
-	t[0][0] = 1;
-	int k = 1; //to count no. of zeroes till ith index of the array
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= sum; j++) {
-			if (i == 0 && j > 0)
-				t[i][j] = 0;
-			if (j == 0 && i > 0) { //instead of putting all t[i][j]=0 for (j == 0 && i > 0) , we do this
-				if (nums[i - 1] == 0) { 
-					t[i][j] = pow(2, k);//if nums[i-1] is zero, simply initialize t[i][j] as pow(2,total zeroes till now)
-					k++;
-				}
-				else
-					t[i][j] = t[i - 1][j]; //else this...
-			}
-		}
-	}
+    int dp[21][40002];
+    int fun(int i, int target, int&n, vector<int>&nums){
+        if(i==n)
+            return target==0;
+        if(dp[i][target+20000] != -1)
+            return dp[i][target+20000];
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= sum; j++) {
-			if (nums[i - 1] <= j)
-				t[i][j] = t[i - 1][j - nums[i - 1]] + t[i - 1][j];
-			else
-				t[i][j] = t[i - 1][j];
-		}
-	}
+        int p = fun(i+1, target-nums[i], n, nums);
+        int m = fun(i+1, target+nums[i], n, nums);
 
-	return t[n][sum];
-}
-    int findTargetSumWays(vector<int>& nums, int diff) {
-        int n= nums.size();
-	int sumOfArray = 0;
-	for (int i = 0; i < n; i++)
-		sumOfArray += nums[i];
-
-	if(((sumOfArray + diff) % 2 != 0)||((sumOfArray + diff)<0))  //if odd or ((sumOfArray + diff)<0), simply return 0 ****IMP*****
-		return 0;
-	else
-		return CountSubsetsWithSum(nums, (sumOfArray + diff) / 2);
-
-    
+        return dp[i][target+20000] = p+m;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        memset(dp, -1, sizeof(dp));
+        return fun(0, target, n, nums);
     }
 };
