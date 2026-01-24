5fb7039dabb5f6454ada9e441bc0794183dfa72e
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2026-01-25 00:44:32 +0530

    Update 06 *IMP_COMMENT* Count of Subsets with given Sum.cpp

--- Dynamic Programming/06 *IMP_COMMENT* Count of Subsets with given Sum.cpp
+++ Dynamic Programming/06 *IMP_COMMENT* Count of Subsets with given Sum.cpp
@@ -1,3 +1,8 @@
+// we have 3 ways here:
+1. WRONG soln
+2. correct soln
+3. optimised soln
+
 // WRONG SOLN:
 // might look correct
 // fails when arr has zeroes (like {0, 4, 0})
@@ -6,6 +11,7 @@
 class Solution {
   public:
     int dp[1001][1001];
+    int mod = 1e9+7;
     
     int func(int i, int&n, int j, int&target, vector<int>&arr){
         if(j==target)
@@ -15,7 +21,7 @@ class Solution {
         if(dp[i][j]!=-1)
             return dp[i][j];
         
-        return dp[i][j] = func(i+1, n, j+arr[i], target, arr) + func(i+1, n, j, target, arr);
+        return dp[i][j] = (func(i+1, n, j+arr[i], target, arr) + func(i+1, n, j, target, arr))%mod;
     }
     int perfectSum(vector<int>& arr, int target) {
         int n = arr.size();
@@ -23,3 +29,40 @@ class Solution {
         return func(0, n, 0, target, arr);
     }
 };
+
+
+// CORRECT SOLN:
+
+class Solution {
+  public:
+    int dp[1001][1001];
+    int mod = 1e9+7;
+    
+    int func(int i, int&n, int j, int&target, vector<int>&arr){
+        // IMP => return ONLY when we traverse all elements
+        // we removed the return of (j==target)
+        if(i==n)
+            return (j==target);
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int take = 0;
+        if(j+arr[i] <= target)
+            take += func(i+1, n, j+arr[i], target, arr);
+        
+        int dont = func(i+1, n, j, target, arr);
+        
+        return dp[i][j] = (take+dont)%mod;
+    }
+    int perfectSum(vector<int>& arr, int target) {
+        int n = arr.size();
+        memset(dp, -1, sizeof(dp));
+        return func(0, n, 0, target, arr);
+    }
+};
+
+
+// OPTIMISED
+// Again 2 rows DP soln
+// reuse rows
+
