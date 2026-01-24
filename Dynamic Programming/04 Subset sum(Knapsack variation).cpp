a07af0638dade858b34190f6f17e531d2f2442ed
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2026-01-24 15:09:51 +0530

    Update 04 Subset sum(Knapsack variation).cpp

--- Dynamic Programming/04 Subset sum(Knapsack variation).cpp
+++ Dynamic Programming/04 Subset sum(Knapsack variation).cpp
@@ -2,50 +2,72 @@
 
 //Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum. 
 
-#include <iostream>
-using namespace std;
+// APPROACH 1 (BAD SPACE COMPLEXITY) => recursive
+// TIME AND SPACE O(N*M)
 
-bool isSubsetPossible(int arr[], int n, int sum) {
-	bool t[n + 1][sum + 1]; // DP - matrix
-	// initialization
-  // here we are setting 1st row and 1st column 
-  // i denotes the size of the array 
-  // j denotes the target sum (subset sum)
-	for (int i = 0; i <= n; i++) { // itereate as long it is less then length of the array
-		for (int j = 0; j <= sum; j++) { 
-			if (i == 0)// when array(i) is empty than there is no meaning of sum of elements so return false
-				t[i][j] = false;
-			if (j == 0) // when sum(j) is zero and there is always a chance of empty subset so return it as true;
-				t[i][j] = true;
-		}
-	}
-// start from 1 since 1st row and column is already considerd 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= sum; j++) {
-			if (arr[i - 1] <= j) 
-        // after taking and element substract from the (sum) i.e -> in {3,8} 3 is taken then we want 11-3=8in the array 
-				t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j]; // either take or(||) do not take 
-			else // if sum is less than array size just leave and increment 
-				t[i][j] = t[i - 1][j];
-		}
-	}
+int dp[201][10001];
+// dp[i][j] indicates if till the i'th index, is it possible to make sum = j
+// dp[i][j] => -1 means not visited, 0 means false, 1 means true
+class Solution {
+  public:
+    bool func(int i, int &n, int j, int& sum, vector<int>&arr){
+        if(j==sum)
+            return true;
+        if(i==n || j>sum)
+            return false;
+        if(dp[i][j]!=-1){
+            return dp[i][j]==1 ? true : false; 
+        }
+        
+        if(j+arr[i] <= sum){
+            dp[i][j] = func(i+1, n, j+arr[i], sum, arr) || func(i+1, n, j, sum, arr);
+        }else{
+            dp[i][j] = func(i+1, n, j, sum, arr);
+        }
+        
+        return dp[i][j];
+    }
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        memset(dp, -1, sizeof(dp));
+        return func(0, n, 0, sum, arr);
+    }
+};
 
-	return t[n][sum]; // at last return T/F
-}
+// BETTER APPROACH:
+// TABULATION
+// TIME: O(N*M)
+// SPACE: O(sum_required) => linear
 
-int main() {
-	int n; cin >> n;
-	int arr[n];
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	int sum; cin >> sum;
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        
+        vector<vector<bool>>dp(2, vector<bool>(sum+1, false));  //NOTE: sum+1
+		// dp[i][j] = is it possible to make sum = j, by including/excluding elemsnts till index i
+        // dp[k][0] = true always (exclude all elements)
+		dp[0][0] = true;
 
-	isSubsetPossible(arr, n, sum) ? cout << "Yes\n" : cout << "No\n";
-	return 0; 
-}
-/*
-Complexity Analysis: 
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<=sum; j++){
+                if(j >= arr[i-1]) //NOTE: we compare with arr[i-1] since dp considers (i=0)...uptill (i=n) 
+				{ 
+                    dp[1][j] = (dp[0][j-arr[i-1]] || dp[0][j]);
+                }
+				else
+				{
+                    dp[1][j] = dp[0][j];
+                }
+            }
 
-Time Complexity: O(sum*n), where sum is the ‘target sum’ and ‘n’ is the size of array.
-Auxiliary Space: O(sum*n), as the size of 2-D array is sum*n.
-*/
+			// dp[i][j] = (dp[i-1][j-arr[i-1]] || dp[i-1][j])
+			// means at one point in time, only (i) and (i-1) are needed for calculations
+			// thus only 2 dp rows are needed, reuse them
+            for(int j=0; j<=sum; j++)
+                dp[0][j] = dp[1][j];
+        }
+        
+        return dp[1][sum];
+    }
+};
