59456c87fbc5ae4e0209de714ff1bebce20be40b
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2026-01-25 13:22:53 +0530

    Update 08 Count the number of subset with given difference.cpp

--- Dynamic Programming/08 Count the number of subset with given difference.cpp
+++ Dynamic Programming/08 Count the number of subset with given difference.cpp
@@ -8,54 +8,38 @@
 // subset_sum1 - subset_sum2 = diff
 //Use the above 2 eqn
 
-#include <bits/stdc++.h>
-using namespace std;
 
-int CountSubsetsWithSum(int arr[], int n, int sum) {
-	int t[n + 1][sum + 1]; // DP - matrix
-	// initialization 
-  // here we are setting 1st row and 1st column 
-  // i denotes the size of the array 
-  // j denotes the target sum (subset sum)
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= sum; j++) {
-			if (i == 0) // when array(i) is empty than there is no meaning of sum of elements so return count of subset as 0;
-				t[i][j] = 0;
-			if (j == 0) // when sum(j) is zero and there is always a chance of empty subset so return count as 1;
-				t[i][j] = 1;
-		}
-	}
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= sum; j++) {
-			if (arr[i - 1] <= j) // when element in the list is less then target sum 
-				t[i][j] = t[i - 1][j - arr[i - 1]] + t[i - 1][j]; // either exclude or inxlude and add both of them to get final count 
-			else
-				t[i][j] = t[i - 1][j]; // exclude when element in the list is greater then the sum 
-		}
-	}
-
-	return t[n][sum]; // finally return the last row and last column element 
-}
-
-int CountSubsetsWithDiff(int arr[], int n, int diff) {
-	int sumOfArray = 0;
-	for (int i = 0; i < n; i++)
-		sumOfArray += arr[i]; // taking sum of the array 
-
-	if ((sumOfArray + diff) % 2 != 0)
-		return 0;
-	else
-		return CountSubsetsWithSum(arr, n, (sumOfArray + diff) / 2);// we will get the number of array(subset) with particular sum 
-}
-
-signed main() {
-	int n; cin >> n;
-	int arr[n];
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	int diff; cin >> diff;
-
-	cout << CountSubsetsWithDiff(arr, n, diff) << endl;
-	return 0;
-}
+class Solution {
+  public:
+    int countPartitions(vector<int>& arr, int diff) {
+        int n = arr.size();
+        int sum = 0;
+        for(auto x:arr)
+            sum+=x;
+        if((sum+diff)%2 ==1)
+            return 0;
+            
+        int req = (sum+diff)/2;
+        if(req > sum)
+            return 0;
+        
+        vector<vector<int>>dp(2, vector<int>(sum+1, 0));
+        dp[0][0] = 1;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<=sum; j++){
+                if(j >= arr[i-1]){
+                    dp[1][j] = dp[0][j] + dp[0][j-arr[i-1]];
+                }else{
+                    dp[1][j] = dp[0][j];
+                }
+            }
+            
+            for(int j=0; j<=sum; j++){
+                dp[0][j] = dp[1][j];
+            }
+        }
+        
+        return dp[1][req];
+    }
+};
