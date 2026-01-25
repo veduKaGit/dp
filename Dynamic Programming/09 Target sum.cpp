aadedb78584607c8837abcaae3e287f93a829b37
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2026-01-25 15:14:31 +0530

    Update 09 Target sum.cpp

--- Dynamic Programming/09 Target sum.cpp
+++ Dynamic Programming/09 Target sum.cpp
@@ -1,8 +1,12 @@
 // https://leetcode.com/problems/target-sum/description/
 
+Again 3 approaches:
+1. recursive (time and space O(N*N))
+2. optimised (space O(N))
+
+
 // in such questions, NEVER include base condition if(target==0)
 // because then we can miss subsets which are forming with trailing zero's
-
 //instead use a single base condition if(i==n)
 
 class Solution {
@@ -23,5 +27,39 @@ public:
         int n = nums.size();
         memset(dp, -1, sizeof(dp));
         return fun(0, target, n, nums);
+    }
+};
+
+
+// optimised:
+
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& arr, int target) {
+        int n = arr.size();
+        int sum = 0;
+        for(auto x:arr)
+            sum += x;
+        if(target>sum || target<(-1*sum))
+            return 0;
+        
+        vector<vector<int>>dp(2, vector<int>(2*sum+1, 0));
+        dp[0][sum] = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<=(2*sum); j++){
+                dp[1][j] = 0;
+                if((j-arr[i-1])>=0)
+                    dp[1][j] += dp[0][j-arr[i-1]];
+                if((j+arr[i-1])<=(2*sum))
+                    dp[1][j] += dp[0][j+arr[i-1]];
+            }
+
+            for(int j=0; j<=(2*sum); j++){
+                dp[0][j] = dp[1][j];
+            }
+        }
+
+        return dp[1][target+sum];
     }
 };
