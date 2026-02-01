86ee6aeae7b3bd8738178bd208406a4196b281ec
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2026-02-01 14:02:10 +0530

    Update 10 Unbounded Knapsack.cpp

--- Dynamic Programming/10 Unbounded Knapsack.cpp
+++ Dynamic Programming/10 Unbounded Knapsack.cpp
@@ -1,35 +1,63 @@
-#include <bits/stdc++.h>
-using namespace std;
+// Again 2 approaches:
+// recursive: time and space O(n*capacity)
+// optimised: time O(n*capacity), space O(2*capacity)
 
-int Un_knapsack(int wt[], int val[], int W, int n) {
-	int t[n + 1][W + 1]; // DP matrix
+//recursive:
+class Solution {
+  public:
+    int dp[1001][1001];
+    int fun(int i, int cur_wt, vector<int>&val, vector<int>&wt, int& cap){
+        int n = val.size();
+        if(i==n)
+            return 0;
+        if(dp[i][cur_wt]!=-1)
+            return dp[i][cur_wt];
+        
+        int take = 0, dont = 0;
+        
+        if(cur_wt + wt[i] <= cap){
+            take = max(val[i]+fun(i, cur_wt+wt[i], val, wt, cap), fun(i+1, cur_wt, val, wt, cap));
+        }
+        dont = fun(i+1, cur_wt, val, wt, cap);
+        
+        return dp[i][cur_wt] = max(take, dont);
+    }
+    int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
+        memset(dp, -1, sizeof(dp));
+        return fun(0, 0, val, wt, capacity);
+    }
+};
 
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= W; j++) {
-			if (i == 0 || j == 0) // base case
-				t[i][j] = 0;
-			else if (wt[i - 1] <= j) { // current wt can fit in bag
-				int val1 = val[i - 1] + t[i][j - wt[i - 1]]; //t[i-1][j - wt[i - 1]] ki jagah t[i][j - wt[i - 1]]...rest is same as 0/1 knapsack
-				int val2 = t[i - 1][j]; 
-				t[i][j] = max(val1, val2);
-			}
-			else if (wt[i - 1] > j) // current wt doesn't fit in bag
-				t[i][j] = t[i - 1][j];
-		}
-	}
 
-	return t[n][W];
-}
+// optimised:
+class Solution {
+  public:
+    int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
+        int n = val.size(), ans = 0;
+        vector<vector<int>>dp(2, vector<int>(capacity+1, 0));
 
-signed main() {
-	int n; cin >> n; // number of items
-	int val[n], wt[n]; // values and wts array
-	for (int i = 0; i < n; i++)
-		cin >> wt[i];
-	for (int i = 0; i < n; i++)
-		cin >> val[i];
-	int W; cin >> W; // capacity
+		// dp[i][j] = MAX value we can make using elements till index i, having sum of weights = j
+		// dp[i][j] = max(dp[i-1][j], dp[i][j-wt[i-1]] + val[i-1]) => VVIMP => read carefully
+		
+		// similar to take/dont-take
+		// max(dont, take)
+		// take = dp[i][j-wt[i-1]] + val[i-1] => IMP!!! => dp[i], NOT dp[i-1], since un-bounded
+        
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<=capacity; j++){
+                if(j-wt[i-1] >= 0){
+                    dp[1][j] = max(dp[0][j], dp[1][j-wt[i-1]]+val[i-1]);
+                }else{
+                    dp[1][j] = dp[0][j];
+                }
+            }
+            
+            for(int j=0; j<=capacity; j++){
+                dp[0][j] = dp[1][j];
+                ans = max(ans, dp[1][j]);
+            }
+        }
 
-	cout << Un_knapsack(wt, val, W, n) << endl;
-	return 0;
-}
+        return ans;
+    }
+};
