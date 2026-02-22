fe54884f8f08d2236e6f968d89a24e481a5b06e1
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2026-02-22 12:46:20 +0530

    Update 24 *IMP* Longest repeating subsequence.cpp

--- Dynamic Programming/24 *IMP* Longest repeating subsequence.cpp
+++ Dynamic Programming/24 *IMP* Longest repeating subsequence.cpp
@@ -4,35 +4,35 @@
 // and are not considering when i==j
 // thats it
 
+// again can be space optimised: 0(N)
 
-#include <bits/stdc++.h>
-using namespace std;
 
-//must watch the video!
+// lets take the single string s
+// dp[i][j] = Longest Repeating Subsequence till index i, and j for s
 
-int LCS(string X, string Y, int n, int m) {
-	int dp[n + 1][m + 1]; // DP - matrix
-
-	for (int i = 0; i <= n; i++)
-		for (int j = 0; j <= m; j++)
-			if (i == 0 || j == 0)
-				dp[i][j] = 0;
-
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= m; j++)
-			if ( X[i - 1]==Y[j - 1] && i!=j )//just add a condition: element at ith posn should NOT be equal to jth posn, rest code is same as LCS
-				dp[i][j] = 1 + dp[i - 1][j - 1];
-			else                           //this condition is NOT put in the else part, otherwise i==j wala part will never be considered
-				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-
-	return dp[n][m];
-}
-
-signed main() {
-	string X; cin >> X;
-	int n = X.length();
-
-	cout << LCS(X, X, n, n) << endl;  // we take LCS with the same string itself
-	return 0;
-}
-// https://www.geeksforgeeks.org/longest-repeating-subsequence/
+class Solution {
+  public:
+    int LongestRepeatingSubsequence(string &s) {
+        int n = s.size();
+        vector<vector<int>>dp(2, vector<int>(n+1, 0));
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i==j){
+                    dp[1][j] = max(dp[0][j], dp[1][j-1]);
+                }else{
+                    if(s[i-1] == s[j-1])  		//IMP => i-1, j-1
+                        dp[1][j] = 1 + dp[0][j-1];
+                    else
+                        dp[1][j] = max(dp[0][j], dp[1][j-1]);
+                }
+            }
+            
+            for(int j=1; j<=n; j++){
+                dp[0][j] = dp[1][j];
+            }
+        }
+        
+        return dp[1][n];
+    }
+};
