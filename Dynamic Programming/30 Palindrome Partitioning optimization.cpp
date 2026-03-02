256ce9c939de6c2fc553395cc283344307ce1e28
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2026-03-02 23:14:54 +0530

    Update 30 Palindrome Partitioning optimization.cpp

--- Dynamic Programming/30 Palindrome Partitioning optimization.cpp
+++ Dynamic Programming/30 Palindrome Partitioning optimization.cpp
@@ -1,1 +1,14 @@
-TODO
+// initially it looks like MCM dp
+// but can be solved in a better way
+
+// time and space: O(n*n)
+
+// dp[i] = minimum cuts needed to partition str[0..i] into palindromic substrings
+// If str[0..i] is a palindrome, no cut is needed (dp[i] = 0)
+
+// for ALL j such that ( str[j] + str[j+1] ... + str[i-1] + str[i] ) is a PALINDROME, then:
+// dp[i] = min(dp[i], 1 + dp[j-1])
+// this is a N*N loop
+
+// so basically, we need to find for ALL str[a, b] if its a palindrome in O(N*N) time
+
