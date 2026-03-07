b1c9910f967e674da28f8735468060f29769bbb6
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2026-03-08 01:08:20 +0530

    Update 44 leetcode_coin_change.cpp

--- Dynamic Programming/44 leetcode_coin_change.cpp
+++ Dynamic Programming/44 leetcode_coin_change.cpp
@@ -5,30 +5,46 @@
 // 1 <= coins[i] <= 2^31 - 1
 // 0 <= amount <= 10^4
 
+=========
 
-//i did in O(n^2)....gave TLE
+// dp[i][cur_amount] = min coins needed to make cur_amount using coins till index i
+// dp[i][cur_amount] = min(1 + dp[i][cur_amount-coins[i]], dp[i-1][cur_amount]);
 
-//this logic: O(n * size_of_coins_vector)
+// why did (1 + dp[i][cur_amount-coins[i]])?
+// why not (1 + dp[i-1][cur_amount-coins[i]])?
+// bcz we can repeat coins
 
-// explanation:
-// https://www.youtube.com/watch?v=H9bfqozjoqs
+// since we use dp i and i-1 => we can OPTIMISE space
 
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
- 
-        vector<int>dp(amount+1,amount+1000);
-        dp[0]=0;
-        for(int i=1;i<=amount;i++)
-            for(int j=0;j<coins.size();j++)
-                if(i-coins[j]>=0)
-                    dp[i]=min(dp[i],1+dp[i-coins[j]]);        
-        return dp[amount] > amount ? -1 : dp[amount];
+        int n = coins.size();
+        vector<vector<int>>dp(2, vector<int>(amount+1, 1e5)); // 1e5 is like INT_MAX here, since max coins we can use is 1e4
+
+        for(int i=0; i<coins.size(); i++){
+            for(int j=0; j<=amount; j++){
+                if(j==0){
+                    dp[1][j] = 0;
+                }else{
+                    if(coins[i] > j)
+                        dp[1][j] = dp[0][j];
+                    else
+                        dp[1][j] = min(1 + dp[1][j-coins[i]], dp[0][j]);
+                }
+            }
+
+            for(int j=0; j<=amount; j++)
+                dp[0][j] = dp[1][j];
+        }
+
+        if(dp[1][amount] >= 1e5)
+            return -1;
+        return dp[1][amount];
     }
 };
 
-
-
+========
 
 // coin change part -2
 // https://leetcode.com/problems/coin-change-2/
