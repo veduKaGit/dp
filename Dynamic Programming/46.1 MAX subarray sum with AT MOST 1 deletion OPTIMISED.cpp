a4c07fee1ba0a9c47df49da58363a6d8f52cbf6a
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2026-03-14 20:59:26 +0530

    Update 46.1 MAX subarray sum with AT MOST 1 deletion OPTIMISED.cpp

--- Dynamic Programming/46.1 MAX subarray sum with AT MOST 1 deletion OPTIMISED.cpp
+++ Dynamic Programming/46.1 MAX subarray sum with AT MOST 1 deletion OPTIMISED.cpp
@@ -1,1 +1,28 @@
+// we can space optimise => O(1)
 
+// Prev soln was Kadanes algo
+// This one is Dp
+
+we maintain 2 dp arrays
+// dp_keep[i] = max subarray sum till index i without deleting any element till now
+// dp_delete[i] = max subarray sum till index i, given that we have deleted an element till now
+
+// dp_keep[i] = max(arr[i], arr[i]+dp_keep[i-1])  => simple kadaned
+// dp_delete[i] = max(dp_keep[i-1], arr[i]+dp_delete[i-1])  => delete this element, element already deleted before index i
+
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        int keep = arr[0];        // no deletion
+        int del = -1*(1e5);       // one deletion, initialise with INT_MIN
+        int ans = arr[0];
+
+        for(int i = 1; i < arr.size(); i++) {
+            del = max(keep, del + arr[i]);
+            keep = max(arr[i], keep + arr[i]);
+            ans = max(ans, max(keep, del));
+        }
+
+        return ans;
+    }
+};
