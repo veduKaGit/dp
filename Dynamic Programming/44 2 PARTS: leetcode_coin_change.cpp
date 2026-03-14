15f69303650438818ebc0769440a48258d1a3d43
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2026-03-14 13:53:38 +0530

    Update 44 2 PARTS: leetcode_coin_change.cpp

--- Dynamic Programming/44 2 PARTS: leetcode_coin_change.cpp
+++ Dynamic Programming/44 2 PARTS: leetcode_coin_change.cpp
@@ -48,29 +48,35 @@ public:
 // coin change part -2
 // https://leetcode.com/problems/coin-change-2/
 
+
+// dp[i][amount] = num ways to make amount using elements till inedx i
+// samek take infinitely/ dont take logic:
+// dp[i][amount] = dp[i][amount-coins[i-1]] + dp[i-1][amount]
+
+// can be OPTIMISED in SPACE!!!
+    
 class Solution {
 public:
+    using ull = unsigned long long;
     int change(int amount, vector<int>& coins) {
-        int n= coins.size();
+        int n = coins.size();
         
-        vector<vector<int>>dp(n+1 , vector<int>(amount+1,0));
-        
-        dp[0][0]=0;
-        
-        for(int i=1;i<=n;i++)
-            dp[i][0]=1;
-        
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=amount;j++)
-            {
-                if(j>=coins[i-1])
-                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];  //NOT dp[i-1][j-coins[i-1]] because we can repeat coins
-                else
-                    dp[i][j]=dp[i-1][j];
+        vector<vector<ull>>dp(2, vector<ull>(amount+1, 0));
+        dp[0][0] = 1;
+        dp[1][0] = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=amount; j++){
+                dp[1][j] = dp[0][j];
+                if(j >= coins[i-1])
+                    dp[1][j] += dp[1][j-coins[i-1]];
             }
+
+            for(int j=1; j<=amount; j++)
+                dp[0][j] = dp[1][j];
         }
-        
-        return dp[n][amount];
+
+        return int(dp[1][amount]);
     }
 };
+
